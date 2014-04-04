512547266bc8166e1dbd469955a6dcab31b586ac
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-04-04 12:01:27 -0700

    server is able to display local content in __dirname

--- server.js
+++ server.js
@@ -7,6 +7,34 @@ mongoose = require( 'mongoose' ); //MongoDB integration
 //create server
 var app =express();
 
+//Connect to database
+// mongoose.connect('mongodb://localhost/user-manager');
+
+
+
+ // Configure server
+app.configure( function() {
+//parses request body and populates request.body
+app.use( express.bodyParser() );
+
+//checks request.body for HTTP method overrides
+app.use( express.methodOverride() );
+
+//perform route lookup based on url and HTTP method
+app.use(app.router);
+
+//Where to serve static content
+app.use( express.static( path.join( application_root, 'main') ) );
+
+//Show all errors in development
+app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+// Routes
+app.get( '/api', function( request, response ) {
+response.send( 'UserManager API is running' );
+});
+
 
 //start server
 var port = 2222;
